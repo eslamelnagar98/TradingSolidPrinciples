f3382ff3590c820e141dd20682398c1d5fbb11b4
eslamelnagar98 <eslamelnagar44@gmailcom>
2023-09-07 13:02:18 +0300

    Add project files.

---
+++ TradingSolidPrinciples/Entities/Trade.cs
@@ -0,0 +1,8 @@
+﻿namespace TradingSolidPrinciples.Entities;
+public class Trade
+{
+    public string SourceCurrency { get; set; }
+    public string DestinationCurrency { get; set; }
+    public float Lots { get; set; }
+    public decimal Price { get; set; }
+}
