3846419bd66fded5bf48e27d097b21da272e0773
eslamelnagar98 <eslamelnagar44@gmailcom>
2023-09-07 17:53:42 +0300

    Last Change In Trader Proccssor

--- TradingSolidPrinciples/TradeProcessor.cs
+++ TradingSolidPrinciples/TradeProcessor.cs
@@ -2,16 +2,15 @@
 namespace TradingSolidPrinciples;
 public class TradeProcessor
 {
-    private static readonly float _lotSize = 100000f;
     private readonly ITradeDataProvider _tradeDataProvider;
     private readonly ITradeStorage _tradeStorage;
     private readonly ITradeParser _tradeParser;
 
     public TradeProcessor(ITradeDataProvider tradeDataProvider, ITradeStorage tradeStorage, ITradeParser tradeParser)
     {
-        _tradeDataProvider = tradeDataProvider;
-        _tradeStorage = tradeStorage;
-        _tradeParser = tradeParser;
+        _tradeDataProvider = tradeDataProvider ?? throw new ArgumentNullException(nameof(tradeDataProvider));
+        _tradeParser = tradeParser ?? throw new ArgumentNullException(nameof(tradeParser));
+        _tradeStorage = tradeStorage ?? throw new ArgumentNullException(nameof(tradeStorage));
     }
     public void ProcessTrades()
     {
